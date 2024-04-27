c1d9349e7d7f4eedef28e46f65d5a4b354f8894f
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-04-27 18:47:54 +0100

    Update chatBot.js

--- Mid-Plugins/chatBot.js
+++ Mid-Plugins/chatBot.js
@@ -1,155 +1,11 @@
 let handler = m => m;
 handler.all = async function (m) {
 
-    if (/^(كود الشهر|كود جوميا|كوبون جوميا|كوبون جيفورس|كود جيفورس)$/i.test(m.text)) { 
- conn.reply(m.chat, `✪ *جوميا:*
-  ⍟ 
-  ⍟
-  ⍟ 
-  ⍟
-  ⍟
-  ⍟
-  
-  ✪ *جي فورص:*
-  ⍟ 
-  ⍟ 
-`, m);
-      } 
       if (/^(cc|سس|ss)$/i.test(m.text)) { 
      m.react(`🤖`);
           } 
-    if (/^(سجوميا)$/i.test(m.text)) { 
-   conn.reply(m.chat, `https://www.jumia.ma/ar/epicerie/?sort=lowest-price&price_discount=50-100&shop_premium_services=shop_express#catalog-listing
-
-https://www.jumia.ma/ar/all-products/?sort=lowest-price&price=9#catalog-listing
-
-https://www.jumia.ma/ar/all-products/?sort=lowest-price&price=10#catalog-listing
-
-https://www.jumia.ma/ar/all-products/?sort=lowest-price&price=0-2#catalog-listing`, m);
-        } 
-  
-      if (/^(سور القران|سور القرأن|سور القرآن)$/i.test(m.text)) { //sin prefijo
-    conn.reply(m.chat, `إليك القائمة بأسماء السور في القرآن الكريم مرقمة:
-
-  1 - الفاتحة (Al-Fatiha)
-  2 - البقرة (Al-Baqara)
-  3 - آل عمران (Aal-E-Imran)
-  4 - النساء (An-Nisa)
-  5 - المائدة (Al-Ma'idah)
-  6 - الأنعام (Al-An'am)
-  7 - الأعراف (Al-A'raf)
-  8 - الأنفال (Al-Anfal)
-  9 - التوبة (At-Tawbah)
-  10 - يونس (Yunus)
-  11 - هود (Hud)
-  12 - يوسف (Yusuf)
-  13 - الرعد (Ar-Ra'd)
-  14 - إبراهيم (Ibrahim)
-  15 - الحجر (Al-Hijr)
-  16 - النحل (An-Nahl)
-  17 - الإسراء (Al-Isra)
-  18 - الكهف (Al-Kahf)
-  19 - مريم (Maryam)
-  20 - طه (Ta-Ha)
-  21 - الأنبياء (Al-Anbiya)
-  22 - الحج (Al-Hajj)
-  23 - المؤمنون (Al-Mu'minun)
-  24 - النور (An-Nur)
-  25 - الفرقان (Al-Furqan)
-  26 - الشعراء (Ash-Shu'ara)
-  27 - النمل (An-Naml)
-  28 - القصص (Al-Qasas)
-  29 - العنكبوت (Al-Ankabut)
-  30 - الروم (Ar-Rum)
-  31 - لقمان (Luqman)
-  32 - السجدة (As-Sajda)
-  33 - الأحزاب (Al-Ahzab)
-  34 - سبأ (Saba)
-  35 - فاطر (Fatir)
-  36 - يس (Ya-Sin)
-  37 - الصافات (As-Saffat)
-  38 - ص (Sad)
-  39 - الزمر (Az-Zumar)
-  40 - غافر (Ghafir)
-  41 - فصلت (Fussilat)
-  42 - الشورى (Ash-Shura)
-  43 - الزخرف (Az-Zukhruf)
-  44 - الدخان (Ad-Dukhan)
-  45 - الجاثية (Al-Jathiya)
-  46 - الأحقاف (Al-Ahqaf)
-  47 - محمد (Muhammad)
-  48 - الفتح (Al-Fath)
-  49 - الحجرات (Al-Hujraat)
-  50 - ق (Qaf)
-  51 - الذاريات (Adh-Dhariyat)
-  52 - الطور (At-Tur)
-  53 - النجم (An-Najm)
-  54 - القمر (Al-Qamar)
-  55 - الرحمن (Ar-Rahman)
-  56 - الواقعة (Al-Waqi'a)
-  57 - الحديد (Al-Hadid)
-  58 - المجادلة (Al-Mujadila)
-  59 - الحشر (Al-Hashr)
-  60 - الممتحنة (Al-Mumtahina)
-  61 - الصف (As-Saff)
-  62 - الجمعة (Al-Jumu'ah)
-  63 - المنافقون (Al-Munafiqun)
-  64 - التغابن (At-Taghabun)
-  65 - الطلاق (At-Talaq)
-  66 - التحريم (At-Tahrim)
-  67 - الملك (Al-Mulk)
-  68 - القلم (Al-Qalam)
-  69 - الحاقة (Al-Haaqqa)
-  70 - المعارج (Al-Ma'arij)
-  71 - نوح (Nuh)
-  72 - الجن (Al-Jinn)
-  73 - المزمل (Al-Muzzammil)
-  74 - المدثر (Al-Muddaththir)
-  75 - القيامة (Al-Qiyama)
-  76 - الإنسان (Al-Insan)
-  77 - المرسلات (Al-Mursalat)
-  78 - النبأ (An-Naba)
-  79 - النازعات (An-Nazi'at)
-  80 - عبس (Abasa)
-  81 - التكوير (At-Takwir)
-  82 - الإنفطار (Al-Infitar)
-  83 - المطففين (Al-Mutaffifin)
-  84 - الانشقاق (Al-Inshiqaq)
-  85 - البروج (Al-Buruj)
-  86 - الطارق (At-Tariq)
-  87 - الأعلى (Al-Ala)
-  88 - الغاشية (Al-Ghashiyah)
-  89 - الفجر (Al-Fajr)
-  90 - البلد (Al-Balad)
-  91 - الشمس (Ash-Shams)
-  92 - الليل (Al-Lail)
-  93 - الضحى (Adh-Dhuha)
-  94 - الشرح (Ash-Sharh)
-  95 - التين (At-Tin)
-  96 - العلق (Al-Alaq)
-  97 - القدر (Al-Qadr)
-  98 - البينة (Al-Bayyina)
-  99 - الزلزلة (Az-Zalzalah)
-  100 - العاديات (Al-Adiyat)
-  101 - القارعة (Al-Qari'a)
-  102 - التكاثر (At-Takathur)
-  103 - العصر (Al-Asr)
-  104 - الهمزة (Al-Humazah)
-  105 - الفيل (Al-Fil)
-  106 - قريش (Quraish)
-  107 - الماعون (Al-Ma'un)
-  108 - الكوثر (Al-Kawthar)
-  109 - الكافرون (Al-Kafirun)
-  110 - النصر (An-Nasr)
-  111 - المسد (Al-Masad)
-  112 - الإخلاص (Al-Ikhlas)
-  113 - الفلق (Al-Falaq)
-  114 - الناس (An-Nas)`, m);
-  }
-          
+        
       return !0;
   };
 
 export default handler
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
