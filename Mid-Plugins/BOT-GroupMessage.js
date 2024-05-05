cc5916ac74222f8520b039caa966d14c88833d62
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-05-05 21:42:28 +0100

    ◆──ᴍɪᴅ-ʙᴏᴛ──◆

---
+++ Mid-Plugins/BOT-GroupMessage.js
@@ -0,0 +1,34 @@
+// ميزة إرسال رسالة موحدة لجميع أعضاء المجموعة في الخاص
+let handler = async (m, {conn, groupMetadata,  usedPrefix, text,command}) => {
+if (!text && !m.quoted) return m.reply("أدخل نص الرسالة?")
+    let get = await groupMetadata.participants.filter(v => v.id.endsWith('.net')).map(v => v.id)
+    let count = get.length;
+    let sentCount = 0;
+
+
+ let { key } = await conn.sendMessage(m.chat, {text: '_Loading_'})
+
+    for (let i = 0; i < get.length; i++) {
+        setTimeout(function() {
+            if (text) {
+                conn.sendMessage(get[i], {
+                    text: text
+                });
+            } else if (m.quoted) {
+                conn.copyNForward(get[i], m.getQuotedObj(), false);
+            } else if (text && m.quoted) {
+                conn.sendMessage(get[i], {
+                    text: text + "\n" + m.quoted.text
+                });
+            }
+            count--;
+            sentCount++;
+            if (count === 0) {
+             m.reply(`*${sentCount}*`);
+            }
+        }, i * 1000); // تأخير كل رسالة لمدة 1 ثانية
+    }
+}
+handler.command = ["send", "sift"]
+handler.group = true
+export default handler
