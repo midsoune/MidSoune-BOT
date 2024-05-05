a7a3aa61d840240ba8624977cb04605586e7ded1
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-05-05 21:58:05 +0100

    ◆──ᴍɪᴅ-ʙᴏᴛ──◆

--- Mid-Plugins/TOOLS-translate.js
+++ Mid-Plugins/TOOLS-translate.js
@@ -1,6 +1,6 @@
 // ميزة الترجمة لأي لغة
 import {translate} from '@vitalets/google-translate-api';
-const handler = async (m, {args}) => {
+const midsoune = async (m, {args}) => {
   if (!args || !args[0]) return m.reply(`*دخل اللنص لي بغيتي تترجمو للعربية أو اللغة + النص لي بغيتي تترجمو للغة أخرى\n\nمثال:\n.${command} Hello\n.${command} fr Hello*`);
   let lang = args[0];
   let text = args.slice(1).join(' ');
@@ -15,5 +15,5 @@ const handler = async (m, {args}) => {
     await conn.reply(m.chat, `*${result.text}*`, null);
   } catch {}
 };
-handler.command = /^(ترجم|translate|trans|trad|ترجمة)$/i
-export default handler
+midsoune.command = /^(ترجم|translate|trans|trad|ترجمة)$/i
+export default midsoune
